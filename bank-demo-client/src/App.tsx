07477e23432bf8a542674c43863abff658799823
Artem Gordienko <arvgord@gmail.com>
2023-02-01 23:14:06 +0700

    feat: add useEffect for getClientListRequest

--- bank-demo-client/src/App.tsx
+++ bank-demo-client/src/App.tsx
@@ -1,26 +1,14 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
+import {useGetList} from "./components/Requests";
 import {ExtractingStrategy} from "@arvgord/bank-demo-api/bankdemo/v1/messages/extracting_strategy_pb";
-import {
-    BankDemoPromiseClient
-} from '@arvgord/bank-demo-api/bankdemo/v1/api/business/bank_demo_grpc_web_pb'
-import {GetClientListRequest} from '@arvgord/bank-demo-api/bankdemo/v1/api/business/bank_demo_pb'
-import {PageRequest} from '@arvgord/bank-demo-api/bankdemo/v1/messages/page_request_pb'
-import {Int32Value} from "google-protobuf/google/protobuf/wrappers_pb";
 
 function App() {
-    const [service] = useState(new BankDemoPromiseClient('http://localhost:8080', null, null))
-    useEffect(() => {
-        const request = new GetClientListRequest()
-        const pageRequest = new PageRequest()
-        pageRequest.setPage(new Int32Value().setValue(0))
-        pageRequest.setSize(new Int32Value().setValue(5))
-        request.setPageRequest(pageRequest)
-        request.setExtractingStrategy(ExtractingStrategy.EXTRACTING_STRATEGY_BATCH)
-        service.getClientList(request, {}).then(m => console.log(m)).catch(e => console.log(e))
-    }, [])
+    const {loading, data, error} = useGetList(0, 100, ExtractingStrategy.EXTRACTING_STRATEGY_EAGER)
+    if (loading) return <h1>Loading</h1>
+    if (error) return <h1>${error}</h1>
     return (
         <div>
-            Test
+          {data.clientsList.length}
         </div>
     );
 }
---
+++ bank-demo-client/src/components/Requests.ts
@@ -0,0 +1,36 @@
+import {useEffect, useState} from 'react';
+import {
+    GetClientListRequest, GetClientListResponse
+} from "@arvgord/bank-demo-api/bankdemo/v1/api/business/bank_demo_pb";
+import {PageRequest} from "@arvgord/bank-demo-api/bankdemo/v1/messages/page_request_pb";
+import {Int32Value} from "google-protobuf/google/protobuf/wrappers_pb";
+import {ExtractingStrategy} from "@arvgord/bank-demo-api/bankdemo/v1/messages/extracting_strategy_pb";
+import {BankDemoPromiseClient} from "@arvgord/bank-demo-api/bankdemo/v1/api/business/bank_demo_grpc_web_pb";
+
+export function useGetList(page: number, size: number, strategy: ExtractingStrategy) {
+    const [data, setData] = useState(new GetClientListResponse().toObject())
+    const [error, setError] = useState()
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        if (!page && !size) return
+        const service = new BankDemoPromiseClient('http://localhost:8080', null, null)
+        const request = new GetClientListRequest()
+        const pageRequest = new PageRequest()
+        pageRequest.setPage(new Int32Value().setValue(page))
+        pageRequest.setSize(new Int32Value().setValue(size))
+        request.setPageRequest(pageRequest)
+        request.setExtractingStrategy(strategy)
+        service.getClientList(request, {})
+            .then(result => result.toObject())
+            .then(setData)
+            .then(() => setLoading(false))
+            .catch(setError)
+    }, [page, size, strategy]);
+
+    return {
+        loading,
+        data,
+        error
+    };
+}
